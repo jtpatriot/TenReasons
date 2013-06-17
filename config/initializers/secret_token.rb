8b0b65b303d22c88ccea3a253631e9dd6c8f4be6
Jarrett Tilford <jtpatriot@me.com>
2013-06-17 16:12:32 -0700

    Commit of main files
    Server is working. Controller home index and generate scaffold Post
    have been done. Home page has a header title.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TenReasons::Application.config.secret_key_base = 'b5c6cc9a43cbb0355a5989f97ada04c7b0c31058431e23fb863ca3a2d0bbf6710791fa4fe7fbbd4bd5c34af033858cd96a39df84d3bf98739d3180e8df184bd2'
